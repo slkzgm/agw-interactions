ccfdc6cbcaed8aef86473f54d97ea4f9f615dd77
slkzgm <contact@slkzgm.com>
2025-03-27 19:00:49 +0100

    adding referer origin

--- src/app/api/magicEden/asks/route.ts
+++ src/app/api/magicEden/asks/route.ts
@@ -39,6 +39,8 @@ export async function GET(request: Request) {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " +
         "Chrome/134.0.0.0 Safari/537.36",
+      "Referer": "https://magiceden.io/",
+      "Origin": "https://magiceden.io",
     },
   });
 
